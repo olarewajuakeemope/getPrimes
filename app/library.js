3a1a855deb81c32e1af65cc654920435efb61210
olarewajuakeemope <olarewajuakeemopeyemi@gmail.com>
2017-03-13 10:57:25 +0100

    library.js supports all test cases

--- app/library.js
+++ app/library.js
@@ -2,23 +2,25 @@
 
 module.exports = {
 
-prime: function(num_limit){
+  prime: function(num_limit) {
 
-var prime_array = [];
-if(num_limit<2)
-    return prime_array;
-for (var counter = 2; counter <= num_limit; counter++) {
+    var prime_array = [];
+    if (typeof num_limit !== "number")
+      return "Invalid input type"
+    if (num_limit < 2)
+      return prime_array;
+    for (var counter = 2; counter <= num_limit; counter++) {
 
-    var notPrime = false;
-    for (var i = 2; i <= counter; i++) {
-        if (counter%i===0 && i!==counter) {
-            notPrime = true;
+      var notPrime = false;
+      for (var i = 2; i <= counter; i++) {
+        if (counter % i === 0 && i !== counter) {
+          notPrime = true;
         }
+      }
+      if (notPrime === false) {
+        prime_array.push(counter);
+      }
     }
-    if (notPrime === false) {
-                prime_array.push(counter);
-    }
- }
-return prime_array;
-}
+    return prime_array;
+  }
 }
